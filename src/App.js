b163be7a7b5483b6c9b6fcab095f4b3350050ef1
Blank67 <hmnshshrm67@gmail.com>
2022-11-29 19:36:51 +0530

    meals summary, available meals and meals added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import MealsSummary from "./components/Meals/MealsSummary";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </React.Fragment>
   );
 }
